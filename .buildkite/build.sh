c46eb4a07954cc6b734f0525c5827a28740bd3bd
Shao Cheng <cheng.shao@tweag.io>
2020-03-19 00:24:31 +0100

    WIP

---
+++ .buildkite/build.sh
@@ -0,0 +1,50 @@
+#!/bin/sh -e
+
+export ASTERIUS_BUILD_OPTIONS=-j8
+export DEBIAN_FRONTEND=noninteractive
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+export LC_CTYPE=C.UTF-8
+export MAKEFLAGS=-j8
+export PATH=/root/.local/bin:$PATH
+
+echo 'deb [check-valid-until=no] http://snapshot.debian.org/archive/debian/20200224T000000Z sid main contrib non-free' > /etc/apt/sources.list
+
+apt update
+apt full-upgrade -y
+apt install -y \
+  automake \
+  cmake \
+  curl \
+  direnv \
+  g++ \
+  gawk \
+  gcc \
+  git \
+  gnupg \
+  libffi-dev \
+  libgmp-dev \
+  libncurses-dev \
+  libnuma-dev \
+  make \
+  openssh-client \
+  python3-pip \
+  sudo \
+  xz-utils \
+  zlib1g-dev
+curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo "deb https://deb.nodesource.com/node_13.x sid main" > /etc/apt/sources.list.d/nodesource.list
+apt update
+apt install -y nodejs
+
+cd /workspaces/asterius
+
+mkdir -p ~/.local/bin
+curl -L https://github.com/commercialhaskell/stack/releases/download/v2.1.3/stack-2.1.3-linux-x86_64.tar.gz | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+curl -L https://downloads.haskell.org/~cabal/cabal-install-3.0.0.0/cabal-install-3.0.0.0-x86_64-unknown-linux.tar.xz | tar xJ -C ~/.local/bin 'cabal'
+
+stack -j8 --no-terminal build --test --no-run-tests
+. .envrc
+ahc-boot
+
+stack test asterius:fib
